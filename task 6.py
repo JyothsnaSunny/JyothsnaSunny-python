1e9b4d7b87b180600a60bc744cd47d3d1a9f35bf
JyothsnaSunny <90957104+JyothsnaSunny@users.noreply.github.com>
2021-09-19 20:22:35 +0530

    Add files via upload
    done by jyothsna pallavi

---
+++ task 6.py
@@ -0,0 +1,8 @@
+#python program to calculate the number of words and the number of characters present in a string.
+a="the bus car dog"
+count=1
+for ch in a:
+    if (ch==" "):
+        count+=1
+print("no.of words in a given string",count)
+        
