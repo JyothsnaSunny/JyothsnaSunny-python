f398b00f63e855a30e5cd866f8b9474d036576e7
JyothsnaSunny <90957104+JyothsnaSunny@users.noreply.github.com>
2021-09-19 20:30:12 +0530

    Add files via upload
    Done by jyothsna pallavi

---
+++ task 1.py  (07-09-21).py
@@ -0,0 +1,103 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> #python program to replace all occurance of 'a' with '$' in a string.
+>>> a="jyothsna pallavi"
+>>> b=a.replace("a","$")
+>>> 
+=== RESTART: C:/Users/Minnu/Desktop/Sunny tasks/task 1.py (07-09-2021).py ===
+jyothsn$ p$ll$vi
+>>> 
+>>> 
+============ RESTART: C:/Users/Minnu/Desktop/Sunny tasks/task2.py ============
+enter astring jyothsna
+Traceback (most recent call last):
+  File "C:/Users/Minnu/Desktop/Sunny tasks/task2.py", line 3, in <module>
+    fc=s[0]#j
+NameError: name 's' is not defined
+>>> 
+============ RESTART: C:/Users/Minnu/Desktop/Sunny tasks/task2.py ============
+enter astring jyothsna
+Traceback (most recent call last):
+  File "C:/Users/Minnu/Desktop/Sunny tasks/task2.py", line 3, in <module>
+    fc=s[0]#j
+NameError: name 's' is not defined
+>>> 
+============ RESTART: C:/Users/Minnu/Desktop/Sunny tasks/task2.py ============
+enter astring jyothsna
+ayothsnj
+Traceback (most recent call last):
+  File "C:/Users/Minnu/Desktop/Sunny tasks/task2.py", line 8, in <module>
+    _
+NameError: name '_' is not defined
+>>> 
+============ RESTART: C:/Users/Minnu/Desktop/Sunny tasks/task2.py ============
+enter a string jyothsna
+ayothsnj
+Traceback (most recent call last):
+  File "C:/Users/Minnu/Desktop/Sunny tasks/task2.py", line 8, in <module>
+    _
+NameError: name '_' is not defined
+>>> 
+============ RESTART: C:/Users/Minnu/Desktop/Sunny tasks/task2.py ============
+enter a string jyothsna
+ayothsnj
+Traceback (most recent call last):
+  File "C:/Users/Minnu/Desktop/Sunny tasks/task2.py", line 8, in <module>
+    _
+NameError: name '_' is not defined
+>>> 
+============ RESTART: C:/Users/Minnu/Desktop/Sunny tasks/task2.py ============
+enter a string pallavi
+iallavp
+Traceback (most recent call last):
+  File "C:/Users/Minnu/Desktop/Sunny tasks/task2.py", line 8, in <module>
+    _
+NameError: name '_' is not defined
+>>> 
+============ RESTART: C:/Users/Minnu/Desktop/Sunny tasks/task2.py ============
+enter a stringpallavi
+iallavp
+>>> 
+=========== RESTART: C:/Users/Minnu/Desktop/Sunny tasks/task 3.py ===========
+enter a stringjyothsna
+no.of vowels in a given string are: 2
+>>> 
+=========== RESTART: C:/Users/Minnu/Desktop/Sunny tasks/task 4.py ===========
+enter a stringelectrical and electronics
+electrical-and-electronics
+>>> 
+=========== RESTART: C:/Users/Minnu/Desktop/Sunny tasks/task 4.py ===========
+length of the string: 5
+>>> 
+=========== RESTART: C:/Users/Minnu/Desktop/Sunny tasks/task 6.py ===========
+no.of words in a given string 4
+>>> 
+>>> 
+=========== RESTART: C:/Users/Minnu/Desktop/Sunny tasks/task 8 .py ===========
+lower cases in name are: 15
+>>> 
+=========== RESTART: C:/Users/Minnu/Desktop/Sunny tasks/task 9.py ===========
+upper cases in name are: 0
+>>> 
+=========== RESTART: C:/Users/Minnu/Desktop/Sunny tasks/task 9.py ===========
+upper cases in name are: 0
+>>> 
+=========== RESTART: C:/Users/Minnu/Desktop/Sunny tasks/task 10.py ===========
+enter a stringjyothsna
+given string is not a palindrome
+>>> 
+=========== RESTART: C:/Users/Minnu/Desktop/Sunny tasks/task12.py ===========
+sa
+ab
+saab
+>>> 
+=========== RESTART: C:/Users/Minnu/Desktop/Sunny tasks/task13.py ===========
+True
+>>> 
+=========== RESTART: C:/Users/Minnu/Desktop/Sunny tasks/task 14.py ===========
+enter a stringjyothsna151
+5
+1
+1
+7
+>>> 
