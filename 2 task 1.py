89620bec49414637b0446afca275a645d0ec922b
JyothsnaSunny <90957104+JyothsnaSunny@users.noreply.github.com>
2021-09-19 20:31:19 +0530

    Add files via upload
    done by jyothsna pallavi

---
+++ 2 task 1.py
@@ -0,0 +1,12 @@
+a=int(input("enter the value of a:"))
+b=int(input("enter the value of b:"))
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+mod=a%b
+print("add:",add)
+print("sub:",sub)
+print("mul:",mul)
+print("mul:",div)
+print("mul:",mod)
