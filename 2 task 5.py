8a4340d1213bda83c80594531b77c71f88b91222
JyothsnaSunny <90957104+JyothsnaSunny@users.noreply.github.com>
2021-09-19 20:35:32 +0530

    Add files via upload
    done by jyothsna pallavi

---
+++ 2 task 5.py
@@ -0,0 +1,10 @@
+print("even numbers")
+b=int(input("enter the ending value"))
+c=2
+for i in range (2,b,2):
+    print(i)
+print("odd numbers")
+b=int(input("enter the ending value"))
+c=1
+for i in range(1,b,2):
+    print(i)
