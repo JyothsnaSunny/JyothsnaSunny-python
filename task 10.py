5c038c6c08778d1a423496013856863fde51796f
JyothsnaSunny <90957104+JyothsnaSunny@users.noreply.github.com>
2021-09-19 20:26:08 +0530

    Add files via upload
    done by jyothsna pallavi

---
+++ task 10.py
@@ -0,0 +1,8 @@
+#python program if a string is palindrome or not
+a=input("enter a string")
+b=a[::-1]
+if(a==b):
+    print("given string is a palindrome")
+else:
+     print("given string is not a palindrome")
+    
