ecd962664b3e2b80dc4ec73bba3e246a9436fd9a
JyothsnaSunny <90957104+JyothsnaSunny@users.noreply.github.com>
2021-09-19 20:23:26 +0530

    Add files via upload
    done by jyothsna pallavi

---
+++ task 7.py
@@ -0,0 +1,17 @@
+#python program to take in two strings and display the larger string without using built-in function
+a= "days"
+b= "bat"
+dcount=0
+ccount=0
+for ch in a:
+    dcount+=1
+print(dcount)
+    bcount+=1
+print(bcount)
+if(dcount>bcount):
+    print("dogs is largest string")
+else:
+     print("cat is largest string")
+    
+    
+    
